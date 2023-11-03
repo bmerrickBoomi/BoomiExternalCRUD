e54d2c301fedd2f0ad5b335e0962e72857baa993
Brian Merrick <brian.merrick@boomi.com>
2023-11-03 15:31:04 -0600

    adding draw

---
+++ CustomBoomi/CRUD/DrawCalendar.cs
@@ -0,0 +1,38 @@
+﻿using CustomBoomi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace CustomBoomi.CRUD
+{
+    public static class DrawCalendar
+    {
+        public static DrawCalendarResponse List()
+        {
+            // Generate random number between [1-9]
+            var rnd = new Random();
+            var count = rnd.Next(1, 10);
+
+            var draws = new DrawCalendarResponse { 
+                Draws = new List<Draw>()
+            };
+
+            for (var i = 0; i < count; i++) {
+
+                var draw = new Draw {
+                    Numbers = new List<int>(),
+                    Date = DateTime.Now.AddDays(rnd.Next(1, 50) * -1)
+                };
+
+                // Generate 5 random numbers between [1-99]
+                for (var j = 0; j < 5; j++) {
+                    draw.Numbers.Add(rnd.Next(1, 100));
+                }
+
+                draws.Draws.Add(draw);
+            }
+
+            return draws;
+        }
+    }
+}
---
+++ CustomBoomi/Models/DrawCalendarResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomBoomi.Models
+{
+    public class DrawCalendarResponse
+    {
+        public List<Draw> Draws { get; set; }
+    }
+
+    public class Draw
+    {
+        public List<int> Numbers { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
